5a893923eb5059dd7494944b1b0381f59d15881c
Anton Gaievyi <anton.gaievyi@gmail.com>
2026-08-06 23:16:51 +0200

    Made a task

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate6432"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-demo-remote"
+  location = "East US"
+}
